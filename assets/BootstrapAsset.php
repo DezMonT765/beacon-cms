1d846f1311627761ca6d87b31e39bbf79847320b
kyvLabs <kyvLabs@gmail.com>
2015-03-01 01:40:39 +0200

    login/register, user crud

---
+++ assets/BootstrapAsset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset bundle for the Twitter bootstrap css files.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class BootstrapAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/bootstrap/dist';
+    public $js = [
+        'js/bootstrap.min.js',
+    ];
+    public $css = [
+        'css/bootstrap.min.css',
+    ];
+}

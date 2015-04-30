990b1b574672c9e7a066a73b5e5a036e1d065d68
kyvLabs <kyvLabs@gmail.com>
2015-04-30 16:05:03 +0300

    environments

---
+++ environments/dev-prod/config/console-local.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'components' => [
+        'db'=>[
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=beacons',
+            'username' => 'galina',
+            'password' => 'Beacons01!',
+            'charset' => 'utf8',
+        ]
+    ],
+];
---
+++ environments/dev-prod/config/main-local.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '',
+        ],
+        'db'=>[
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=beacons',
+            'username' => 'galina',
+            'password' => 'Beacons01!',
+            'charset' => 'utf8',
+        ]
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
